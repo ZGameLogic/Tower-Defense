c804b5495048f6e850cd1ee9650d2b35cdae6281
Ben Shabowski <benshabowski@gmail.com>
2024-03-30 20:06:57 -0500

    Getting the script hooked in correctly

---
+++ Assets/Scripts/Main_Menu_Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Button_Clicked : MonoBehaviour
+{
+   public void OnPlayGameClicked()
+   {
+      print("Play game Clicked");
+   }
+
+   public void OnSettingsClicked()
+   {
+      print("Settings Clicked");
+   }
+
+   public void OnQuitGameClicked()
+   {
+      print("Quit game Clicked");
+   }
+}
